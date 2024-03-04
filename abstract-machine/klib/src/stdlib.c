fe3e4df26e997c71ead3bd20dadc7798b81a5d4a
zhaomaosheng <zmshahaha@gmail.com>
2024-03-04 19:22:48 +0800

    add malloc

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,12 +29,21 @@ int atoi(const char* nptr) {
   return x;
 }
 
+#define ALIGN 8
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  static void *addr = NULL; // heap_start
+
+  if (addr == NULL) {
+    addr = heap.start;
+  }
+
+  void *ret = addr;
+  addr = (void *)ROUNDUP(addr + size, ALIGN);
+  return ret;
 #endif
   return NULL;
 }
