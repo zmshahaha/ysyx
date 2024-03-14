fbd0c9a83124bd1e3260ca2efe48847c60010c69
zhaomaosheng <zmshahaha@gmail.com>
2024-03-14 17:59:32 +0800

    add exit and yield syscall
    TODO: add strace

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,13 @@
 #include <common.h>
 #include "syscall.h"
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case SYS_exit: halt(0); break;
+    case SYS_yield: yield(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
