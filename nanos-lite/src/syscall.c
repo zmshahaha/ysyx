74460f680108f3f496711d7f370de8a49698108f
zhaomaosheng <zmshahaha@gmail.com>
2024-03-14 20:54:22 +0800

    add strace

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,8 +9,16 @@
  * GPRx: syscall return val
  */
 void do_syscall(Context *c) {
+  /* avoid overlay syscall param when calling function ?? */
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+
+#ifdef CONFIG_STRACE
+  Log("syscall %d with args 0x%x 0x%x 0x%x\n", a[0], a[1], a[2], a[3]);
+#endif
 
   switch (a[0]) {
     case SYS_exit: halt(0); break;
