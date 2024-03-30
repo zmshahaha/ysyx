a6ba286de66e21a095d5d1e17fc418ba8c4f7b33
zhaomaosheng <zmshahaha@gmail.com>
2024-03-31 00:09:56 +0800

    apply va in nemu

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -17,13 +17,16 @@
 #include <memory/paddr.h>
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_IFETCH);
+  return paddr_read(paddr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_READ);
+  return paddr_read(paddr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  paddr_write(addr, len, data);
+  paddr_t paddr = isa_mmu_translate(addr, len, MEM_TYPE_WRITE);
+  paddr_write(paddr, len, data);
 }
